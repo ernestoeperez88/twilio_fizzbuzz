6878e57e547ef69e0265462d4422faa035c4c8cd
Ernesto Ezequiel Perez <ernestoeperez88@gmail.com>
2016-02-27 14:38:36 -0800

    Initial Flask and Twilio setup.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Twilio FizzBuzz',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Play FizzBuzz through your phone using the Twilio API.',
+      author='Ernesto Ezequiel Perez',
+      author_email='ernestoeperez88@berkeley.edu',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask>=0.8', 'twilio>=3.3.6'],
      )
