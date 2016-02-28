5ea92c65136bc845580feecb498f881b54ed2de1
Ernesto Ezequiel Perez <ernestoeperez88@gmail.com>
2016-02-27 19:35:17 -0800

    Implemented PhoneBuzz.

--- flaskapp.py
+++ flaskapp.py
@@ -7,15 +7,28 @@ app = Flask(__name__)
 def index():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    with resp.gather(action="/handle-input", method="POST", numDigits=3) as g:
-        g.say("Please enter a number to start playing")
+    with resp.gather(action="/handle-input", method="POST", numDigits=2) as g:
+        g.say("Please enter a two digit number to start playing")
     return str(resp)
 
 @app.route("/handle-input", methods=['GET', 'POST'])
 def handle_input():
-    input_num = request.values.get('Digits', None)
+    input_num = int(request.values.get('Digits', None))
+    message = ''
+
+    # FizzBuzz 
+    for num in range(1, input_num + 1):
+        next = ''
+        if num % 3 == 0:
+            next += 'Fizz'
+        if num % 5 == 0:
+            next += 'Buzz'
+        if not next:
+            next = str(num)
+        message += next + ' '
+
     resp = twilio.twiml.Response()
-    resp.say("You entered " + input_num)
+    resp.say(message)
     return str(resp)
 
 if __name__ == "__main__":
