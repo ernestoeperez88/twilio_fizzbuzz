9115402decb442e306d0432737ff6d63fb8f22b5
Ernesto Ezequiel Perez <ernestoeperez88@gmail.com>
2016-02-27 20:44:16 -0800

    Implemented PhoneBuzz Dialing.

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,31 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import twilio.twiml
+from twilio.rest import TwilioRestClient
  
 app = Flask(__name__)
  
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
+    return render_template('index.html')
+
+@app.route("/", methods=['POST'])
+def dial():
+    phone_num = "+1" + request.form['pNumber']
+    phonebuzz_num = "+17606711394"
+
+    # Credentials
+    account_sid = "ACd0a9af9570cb3204b46a1c874003c045"
+    auth_token = "61cc15906b94828995d98e608bbfb3a9"
+    client = TwilioRestClient(account_sid, auth_token)
+ 
+    # Making call
+    call = client.calls.create(to=phone_num,  # Any phone number
+                           from_=phonebuzz_num, # Must be a valid Twilio number
+                           url="http://twiliofizzbuzz-ernestoeperez.rhcloud.com/play-phonebuzz")
+    print call.sid
+
+@app.route("/play-phonebuzz", methods=['GET', 'POST'])
+def play_phoneBuzz():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
     with resp.gather(action="/handle-input", method="POST", numDigits=2) as g:
