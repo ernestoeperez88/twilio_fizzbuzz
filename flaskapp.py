3a7512f8408932fa104d9daf2df72e12b52a3d53
Ernesto Ezequiel Perez <ernestoeperez88@gmail.com>
2016-02-27 19:14:39 -0800

    Implemented Gather method.

--- flaskapp.py
+++ flaskapp.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 import twilio.twiml
  
 app = Flask(__name__)
  
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
+def index():
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
- 
+    with resp.gather(action="/handle-input", method="POST", numDigits=3) as g:
+        g.say("Please enter a number to start playing")
     return str(resp)
- 
+
+@app.route("/handle-input", methods=['GET', 'POST'])
+def handle_input():
+    input_num = request.values.get('Digits', None)
+    resp = twilio.twiml.Response()
+    resp.say("You entered " + input_num)
+    return str(resp)
+
 if __name__ == "__main__":
     app.run(debug=True)
